461ab2fa038f910f534b24ed07b9b30db5a4cb2d
Vegard Haneberg <vegahane@stud.ntnu.no>
2020-02-10 11:00:08 +0100

    test commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Jake er nub!</Text>
+      <Text>Jake er nub!few test</Text>
     </View>
   );
 }
