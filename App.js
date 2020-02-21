f5fffe97177f0b7d2fa5a24fae31de473973ffbb
Syverkok <syverskok@hotmail.com>
2020-02-21 14:33:10 +0100

    keen på å mekke

--- App.js
+++ App.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-<<<<<<< HEAD
-import { StyleSheet, Text, View, Dimensions, Button, TextInput, TouchableWithoutFeedback , TouchableOpacity, Image } from 'react-native';
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style = {styles.container}>
-        <View style={styles.container2}>
-            <Text style = {styles.header}> SPOTIT </Text>
-        </View>
-        <View style={styles.container}>
-          <MapView style={styles.mapStyle}
-=======
 import {Text, View, Dimensions} from 'react-native';
 import {Header, Button, Body, Title, Fab, Icon, Left, Right} from 'native-base'
 
@@ -37,39 +25,19 @@ export default class App extends React.Component {
         </Header>
 
         <MapView style={styles.mapStyle}
->>>>>>> dev
               initialRegion = {{
                 latitude:63.428104,
                 longitude:10.388036,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
               }}
-<<<<<<< HEAD
-              showsUserLocation
-          >
-=======
               showsUserLocation>
 
->>>>>>> dev
             <MapView.Marker draggable
                 coordinate= {{
                   latitude:63.428104,
                   longitude:10.388036
                 }}
-<<<<<<< HEAD
-                title = "Skjer"
-            />
-            <TouchableWithoutFeedback  onPress={() => alert('Do something!')}>
-              <Image
-                style={styles.logo}
-                source={require('./myButton.png')}
-              />
-           </TouchableWithoutFeedback >
-
-          </MapView>
-
-        </View>
-=======
                 title = "Skjer"/>
 
         </MapView>
@@ -83,53 +51,12 @@ export default class App extends React.Component {
         style={{backgroundColor: 'blue'}}>
             <Icon name="camera"/>
         </Fab>
->>>>>>> dev
       </View>
     );
   }
 }
 
 
-<<<<<<< HEAD
-const styles = StyleSheet.create({
-  logo: {
-   height: 70,
-   width: 70,
-   position: 'absolute',
-   bottom: 2,
-   right: 145
-
- },
-  submitButton: {
-    position: 'absolute',
-    bottom:0,
-    left:0,
-    color: 'red'
-},
-  container: {
-    flex: 4,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  container2: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  header: {
-    fontFamily: 'Baskerville-Bold',
-    fontSize: 50,
-    top: -30,
-  },
-
-});
-=======
 const styles = {
   container: {
     flex: 1,
@@ -144,4 +71,3 @@ const styles = {
     height: Dimensions.get('window').height,
   }
 };
->>>>>>> dev
