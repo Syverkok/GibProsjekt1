5e36479cfcf55dfe3bfb19eb2a637488fe446dbb
Vegard Haneberg <vegahane@stud.ntnu.no>
2020-04-19 15:41:30 +0200

    push

--- App.js
+++ App.js
@@ -4,12 +4,10 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import AboutScreen from './Screens/AboutScreen';
 import HomeScreen from './Screens/HomeScreen';
-import CameraScreen from './Screens/CameraScreen';
 
 const Container = createStackNavigator({
   Home: HomeScreen,
   About: AboutScreen,
-  Camrea: CameraScreen
 },
 {
   initialRouteName: 'Home',
