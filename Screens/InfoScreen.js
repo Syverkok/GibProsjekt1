24b0267bf6fd01f456b54bde43ab11ee875dbade
madsrindal <madseri@stud.ntnu.no>
2020-04-24 17:13:11 +0200

    Publish screen android

--- Screens/InfoScreen.js
+++ Screens/InfoScreen.js
@@ -50,7 +50,6 @@ export default class InfoScreen extends Component {
     render() {
         return (
             <Container>
-            <Header />
             <Content>
               <Form>
                 <Picker
