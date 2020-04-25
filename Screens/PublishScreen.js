36a4cebcf0ccd7ee7381164a1a242600b02e3de8
madsrindal <madseri@stud.ntnu.no>
2020-04-25 18:30:31 +0200

    Fix bug picker

--- Screens/PublishScreen.js
+++ Screens/PublishScreen.js
@@ -184,7 +184,7 @@ const styles = StyleSheet.create({
         //backgroundColor: 'yellow'
     },
     picturefield: {
-        flex: 3,
+        flex: 2,
         alignItems: 'center',
         justifyContent: 'center',
         //backgroundColor: 'darkslateblue',
